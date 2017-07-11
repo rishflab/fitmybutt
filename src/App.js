62402c9389dd8e046cc130b9e3d305c450a9fb7b
Rishab Sharma <rishflab@hotmail.com>
2017-07-12 07:49:05 +1000

    working on CSS

--- src/App.js
+++ src/App.js
@@ -9,10 +9,33 @@ class App extends Component {
  
   render() {
     return (
-      <div className="App">
-        <Search/>
-        <Login/>
+    <div>
+      <div className='menu-container'>
+        <div className="menu">
+          <div className='brand'>Brand</div>
+          <div className='links'>   
+            <div className='signup'>Sign Up</div>
+            <div className='login'>Login</div>
+          </div>
+        </div>
       </div>
+      <div className='header-container'>
+        <div className='header'>
+          <div className='subscribe'>Subscribe</div>
+          <div className='logo'>
+            gergerg
+          </div>
+          <div className='social'>
+            wrefwefw
+          </div>
+        </div>
+      </div>
+      </div>
+      // <div className="App">
+      //   <Search/>
+      //   <Login/>
+      // </div>
+  
     );
   }
 }
