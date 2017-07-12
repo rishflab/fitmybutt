514dc8f6df2bfea9c174c22ffbd80526ec467040
Rishab Sharma <rishflab@hotmail.com>
2017-07-13 07:52:39 +1000

    flexbox learning

--- src/App.js
+++ src/App.js
@@ -10,32 +10,29 @@ class App extends Component {
   render() {
     return (
     <div>
-      <div className='menu-container'>
-        <div className="menu">
-          <div className='brand'>Brand</div>
-          <div className='links'>   
-            <div className='signup'>Sign Up</div>
-            <div className='login'>Login</div>
-          </div>
+      <div className='navbar'>
+        <div className='menu'>
+          <div className='nav-item'>Menu</div>
+        </div>
+        <div className='links'>   
+          <div className='nav-item'>Sign Up</div>
+          <div className='nav-item'>Login</div>
         </div>
       </div>
-      <div className='header-container'>
-        <div className='header'>
-          <div className='subscribe'>Subscribe</div>
-          <div className='logo'>
-            gergerg
-          </div>
-          <div className='social'>
-            wrefwefw
-          </div>
+
+      <div className='container'>
+       
+        <div className="content">
+          ergreg
         </div>
+       
+        <div className="sidebar">
+          dgt
+        </div>
+
       </div>
-      </div>
-      // <div className="App">
-      //   <Search/>
-      //   <Login/>
-      // </div>
-  
+    </div>
+ 
     );
   }
 }
